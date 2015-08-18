4cae15fc6fdaf1ac19939fafa1eb0451babd0d12
Oliver Paisley <oliverjdpaisley@gmail.com>
2015-08-17 18:10:04 -0700

    testing w3c validator (meta utf-8 tag)

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-// gulpfile.js
 var gulp         = require('gulp'),
     express      = require('express'),
     gutil        = require('gulp-util'),
@@ -9,20 +8,44 @@ var gulp         = require('gulp'),
     shell        = require('gulp-shell'),
     livereload   = require('gulp-livereload'),
     lr           = require('tiny-lr'),
+    minifyHTML   = require('gulp-minify-html'),
+    uncss        = require('gulp-uncss'),
+    glob         = require('glob'),
+    importCss    = require('gulp-import-css'),
     server       = lr();
 
-gulp.task('sass', function() {
-  return gulp.src('sass/**/*.scss')
-    .pipe(sass())
-    .pipe(autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ))
-    .pipe(minifycss())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('public/css'))
-    .pipe(gulp.dest('_site/public/css'))
-    .pipe(livereload(server));
+gulp.task('jekyll', function() {
+  return gulp.src('index.html', { read: false })
+    .pipe(shell(['jekyll build --watch']
+  ));
 });
 
-gulp.task('build', ['sass'], shell.task([ 'jekyll build --watch' ]));
+gulp.task('sass', ['jekyll'], function() {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(importCss())
+    .pipe(autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ))
+    .pipe(uncss({
+           html: glob.sync("_site/**/*.html"),
+           ignore: ['label.active', '.dark-mode', 'span.tweet-time']
+       }))
+    .pipe(minifycss({keepBreaks:false}))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('_site/css'))
+});
+
+gulp.task('html', ['jekyll'], function() {
+    return gulp.src('_site/**/*.html')
+        .pipe(minifyHTML({
+            quotes: true
+        }))
+        .pipe(gulp.dest('_site/'));
+});
+
+gulp.task('build',
+          ['jekyll'],
+          ['sass'],
+          ['html']);
 
 // Watch and rebuild on change
 gulp.task('default', function() {
